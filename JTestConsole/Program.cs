331ad74eacf9b7d6de72e209bb83392c08a995b5
louhuahua <lhhzll@126.com>
2021-09-28 17:25:50 +0800

    JConsole Start

---
+++ JTestConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Jenkins Test");
+            Console.ReadLine();
+        }
+    }
+}
